ad1a90f89a1a309d5f89d6333b025ce611dce165
Kasaev Alexei <kasaev_97@mail.ru>
2024-01-30 21:36:57 +0400

    homework 1

--- homework-1/create_tables.sql
+++ homework-1/create_tables.sql
@@ -1,1 +1,41 @@
 -- SQL-команды для создания таблиц
+CREATE TABLE employees
+(
+    employee_id int PRIMARY KEY,
+    first_name varchar(100) NOT NULL,
+	last_name varchar(100) NOT NULL,
+	title varchar(100) NOT NULL,
+	birth_date varchar(100) NOT NULL,
+	notes varchar(500) NOT NULL
+);
+
+CREATE TABLE customers
+(
+    customer_id varchar(10) PRIMARY KEY,
+    company_name varchar(100) NOT NULL,
+	contact_name varchar(100) NOT NULL
+);
+
+CREATE TABLE orders
+(
+    order_id int PRIMARY KEY,
+    customer_id varchar(10) NOT NULL REFERENCES customers(customer_id),
+	employee_id int NOT NULL REFERENCES employees(employee_id),
+	order_date varchar(100) NOT NULL,
+	ship_city varchar(100) NOT NULL
+)
+
+ALTER TABLE employees
+ALTER COLUMN employee_id TYPE INT,
+ALTER COLUMN birth_date TYPE VARCHAR(100),
+ALTER COLUMN notes TYPE VARCHAR(500);
+
+ALTER TABLE customers
+ALTER COLUMN customer_id TYPE VARCHAR(10);
+
+ALTER TABLE orders
+ALTER COLUMN order_id TYPE INT,
+ALTER COLUMN customer_id TYPE VARCHAR(10),
+ALTER COLUMN employee_id TYPE INT,
+ALTER COLUMN order_date TYPE VARCHAR(100),
+ALTER COLUMN ship_city TYPE VARCHAR(100)
--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,35 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import csv
+
+import psycopg2
+
+FILE_CUSTOMERS_DATA = 'north_data/customers_data.csv'
+FILE_EMPLOYEES_DATA = 'north_data/employees_data.csv'
+FILE_ORDERS_DATA = 'north_data/orders_data.csv'
+
+connect = psycopg2.connect(host='localhost', database='north', user='postgres', password='RozaVetrov792143')
+
+
+def insert_data_to_sql(filename, tablename):
+    try:
+        with open(filename, 'r', encoding='UTF-8') as file:
+            data = csv.DictReader(file)
+            with psycopg2.connect(host="localhost", database="north", user="postgres", password="2182") as connit:
+                with connit.cursor() as curs:
+                    for row in data:
+                        row_data = []
+                        values_designation = []
+                        for key in list(row.keys()):
+                            row_data.append(row[key])
+                            values_designation.append('%s')
+                        curs.execute(f"INSERT INTO {tablename} VALUES ({', '.join(values_designation)})",
+                                     tuple(row_data))
+                        row_data.clear()
+                        values_designation.clear()
+    finally:
+        connect.close()
+
+
+insert_data_to_sql(FILE_CUSTOMERS_DATA, 'customers')
+insert_data_to_sql(FILE_EMPLOYEES_DATA, 'employees')
+insert_data_to_sql(FILE_ORDERS_DATA, 'orders')
